89c4d6258b5d828b2a052ea97c9e307cdc3280d4
HarukiSakurai105 <hoanglong20041@gmail.com>
2025-03-09 11:51:54 +0700

    Qlsach

--- src/test/java/com/example/quanlysach/QuanlysachApplicationTests.java
+++ src/test/java/com/example/quanlysach/QuanlysachApplicationTests.java
@@ -2,14 +2,15 @@ package com.example.quanlysach;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc  // 🔥 Thêm annotation này để tự động cấu hình MockMvc
 class QuanlysachApplicationTests {
 
     @Autowired
